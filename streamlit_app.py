aa70ce1c167cdaa0242f5ac3285d37212ea5ca4f
TWamslerSnowflake <141998376+TWamslerSnowflake@users.noreply.github.com>
2023-08-11 09:56:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-import streamlit as st
- 
-st.title('My Parents New Healthy Diner')
+import streamlit
 
-st.header('Breakfest Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-boiled Free-Range Egg')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
